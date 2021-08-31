88a05f23143ec60e0c1993b9ba78fa8268266e27
unknown <moreakshay161@gmail.com>
2021-08-31 10:46:05 +0530

    Todo list

---
+++ src/MyComponents/About.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export const About = () => {
+    return (
+        <div>
+            <h3>This is about component</h3>
+            <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Animi perferendis quam repellendus molestiae fuga incidunt aspernatur earum omnis iusto hic?</p>
+        </div>
+    )
+}
